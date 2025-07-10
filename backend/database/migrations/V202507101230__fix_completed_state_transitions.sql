da53f3fec0ea370d42a8a4bf7a6029c8de31d405
Qingyu255 <qingyu2555@gmail.com>
2025-07-10 12:51:30 +0800

    ORA-28 Fix completed state fsm transition

---
+++ backend/database/migrations/V202507101230__fix_completed_state_transitions.sql
@@ -0,0 +1,2 @@
+INSERT INTO senior_sync.fsm_transitions (campaign_name, trigger, source_state, dest_state, guard_name, action_name) VALUES
+('lodging_request', 'FIRSTCHATOPEN', 'COMPLETED', 'START', null, 'autoRestartAction');
