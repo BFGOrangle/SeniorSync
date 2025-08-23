7201b9c7a51169751e0101d9d0b27e2c7b074881
Lim Jitt Hing <jhlim.2023@smu.edu.sg>
2025-08-23 10:53:53 +0800

    chore(NO-TICKET): vendor app endpoint (#85)

--- backend/seniorsync/src/main/java/orangle/seniorsync/common/config/SecurityConfig.java
+++ backend/seniorsync/src/main/java/orangle/seniorsync/common/config/SecurityConfig.java
@@ -49,6 +49,7 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll() // Allow preflight requests
                         .requestMatchers("/api/auth/**").permitAll() // Public auth endpoints
                         .requestMatchers("/api/test/**").permitAll() // Test endpoints
+                        .requestMatchers("/api/vendor-applications").permitAll()
                         .requestMatchers("/swagger", "/swagger-ui/**", "/api-docs/**").permitAll() // Swagger UI
                         .anyRequest().authenticated()
                 )
